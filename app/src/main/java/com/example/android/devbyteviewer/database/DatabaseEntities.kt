8f49814bf66703ee92ec4df150c21d18449258b8
ruo20 <ruo2019@outlook.com>
2021-06-29 10:51:38 -0700

    added VideoDao

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.android.devbyteviewer.domain.DevByteVideo
 
@@ -28,14 +29,14 @@ import com.example.android.devbyteviewer.domain.DevByteVideo
 /**
  * DatabaseVideo represents a video entity in the database.
  */
+@Entity
 data class DatabaseVideo constructor(
     @PrimaryKey
     val url: String,
     val updated: String,
     val title: String,
     val description: String,
-    val thumbnail: String
-)
+    val thumbnail: String)
 
 /**
  * Map DatabaseVideos to domain entities
@@ -44,10 +45,9 @@ fun List<DatabaseVideo>.asDomainModel(): List<DevByteVideo> {
     return map {
         DevByteVideo(
             url = it.url,
-            updated = it.updated,
             title = it.title,
             description = it.description,
-            thumbnail = it.thumbnail
-        )
+            updated = it.updated,
+            thumbnail = it.thumbnail)
     }
 }
--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -58,10 +58,10 @@ data class NetworkVideo(
 fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
     return videos.map {
         DatabaseVideo(
-                title = it.title,
-                description = it.description,
-                url = it.url,
-                updated = it.updated,
-                thumbnail = it.thumbnail)
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
     }
 }
