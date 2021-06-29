d59503090fc6a7732f045dc0cc0c84a980ca29bf
ruo20 <ruo2019@outlook.com>
2021-06-29 10:44:35 -0700

    added database object

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -16,8 +16,38 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.DevByteVideo
+
 
 /**
  * Database entities go in this file. These are responsible for reading and writing from the
  * database.
  */
+
+/**
+ * DatabaseVideo represents a video entity in the database.
+ */
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+/**
+ * Map DatabaseVideos to domain entities
+ */
+fun List<DatabaseVideo>.asDomainModel(): List<DevByteVideo> {
+    return map {
+        DevByteVideo(
+            url = it.url,
+            updated = it.updated,
+            title = it.title,
+            description = it.description,
+            thumbnail = it.thumbnail
+        )
+    }
+}
