b47209111b32c983b28a82791cb98d05d80d85a0
ruo20 <ruo2019@outlook.com>
2021-06-29 17:43:54 -0700

    implement doWork()

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
@@ -3,9 +3,21 @@ package com.example.android.devbyteviewer.work
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.conkermobileX.plywood.logDebug
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
 
-class RefreshDataWorker(appContext: Context, parans: WorkerParameters) : CoroutineWorker(appContext, parans) {
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
     override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        try {
+            repository.refreshVideos()
+            logDebug("Work request for sync is run")
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
         return Result.success()
     }
 }
